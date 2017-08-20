924df46123fcb92bf40abfbca7635c80c7ac6122
sandstranger <sandstranger@gmail.com>
2017-08-20 17:47:11 +0400

    MTK patches-CORE # Conflicts: #	healthd/BatteryMonitor.cpp
    # Conflicts:
    #	healthd/BatteryMonitor.cpp (reverted from commit b87336928846b370b89317b4c5cdca839424199d)

--- healthd/healthd.cpp
+++ healthd/healthd.cpp
@@ -37,8 +37,25 @@
 using namespace android;
 
 // Periodic chores intervals in seconds
-#define DEFAULT_PERIODIC_CHORES_INTERVAL_FAST (24 * 60 * 60)
-#define DEFAULT_PERIODIC_CHORES_INTERVAL_SLOW (24 * 60 * 60)
+#ifndef BOARD_PERIODIC_CHORES_INTERVAL_FAST
+ #ifdef QCOM_HARDWARE
+  #define DEFAULT_PERIODIC_CHORES_INTERVAL_FAST (60 * 10)
+ #else
+  #define DEFAULT_PERIODIC_CHORES_INTERVAL_FAST (60 * 1)
+ #endif
+#else
+  #define DEFAULT_PERIODIC_CHORES_INTERVAL_FAST (BOARD_PERIODIC_CHORES_INTERVAL_FAST)
+#endif
+
+#ifndef BOARD_PERIODIC_CHORES_INTERVAL_SLOW
+ #ifdef QCOM_HARDWARE
+  #define DEFAULT_PERIODIC_CHORES_INTERVAL_SLOW -1
+ #else
+  #define DEFAULT_PERIODIC_CHORES_INTERVAL_SLOW (60 * 10)
+ #endif
+#else
+  #define DEFAULT_PERIODIC_CHORES_INTERVAL_SLOW (BOARD_PERIODIC_CHORES_INTERVAL_SLOW)
+#endif
 
 static struct healthd_config healthd_config = {
     .periodic_chores_interval_fast = DEFAULT_PERIODIC_CHORES_INTERVAL_FAST,
@@ -55,8 +72,6 @@ static struct healthd_config healthd_config = {
     .batteryChargeCounterPath = String8(String8::kEmptyString),
     .batteryFullChargePath = String8(String8::kEmptyString),
     .batteryCycleCountPath = String8(String8::kEmptyString),
-    .batteryCurrentMaxPath = String8(String8::kEmptyString),
-    .batteryVoltageMaxPath = String8(String8::kEmptyString),
     .energyCounter = NULL,
     .boot_min_cap = 0,
     .screen_on = NULL,
@@ -299,12 +314,17 @@ static void wakealarm_init(void) {
 }
 
 static void healthd_mainloop(void) {
+    int nevents = 0;
     while (1) {
         struct epoll_event events[eventct];
-        int nevents;
         int timeout = awake_poll_interval;
         int mode_timeout;
 
+        /* Don't wait for first timer timeout to run periodic chores */
+        if (!nevents)
+            periodic_chores();
+
+        healthd_mode_ops->heartbeat();
 
         mode_timeout = healthd_mode_ops->preparetowait();
         if (timeout < 0 || (mode_timeout > 0 && mode_timeout < timeout))
@@ -321,10 +341,6 @@ static void healthd_mainloop(void) {
             if (events[n].data.ptr)
                 (*(void (*)(int))events[n].data.ptr)(events[n].events);
         }
-        if (!nevents)
-            periodic_chores();
-
-        healthd_mode_ops->heartbeat();
     }
 
     return;
@@ -380,6 +396,9 @@ int main(int argc, char **argv) {
         KLOG_ERROR("Initialization failed, exiting\n");
         exit(2);
     }
+
+    periodic_chores();
+    healthd_mode_ops->heartbeat();
 
     healthd_mainloop();
     KLOG_ERROR("Main loop terminated, exiting\n");
