4805ebb02bdf972065d86a7cb8fe7aac53d1475d
sandstranger <sandstranger@gmail.com>
2017-09-17 14:15:37 +0400

    MTK patches-CORE (reverted from commit c17d47f8a99a669a91b067962994483a7dec3cd9)

--- init/init.cpp
+++ init/init.cpp
@@ -85,7 +85,7 @@ int have_console;
 std::string console_name = "/dev/console";
 static time_t process_needs_restart;
 
-const char *ENV[64];
+const char *ENV[32];
 
 bool waiting_for_exec = false;
 
