1577d5f8994e4617e5443677d4ec3475cdef5769
sandstranger <sandstranger@gmail.com>
2017-10-17 21:36:35 +0400

    core patch

--- init/service.cpp
+++ init/service.cpp
@@ -373,9 +373,10 @@ bool Service::Start() {
         }
         if (rc == 0 && scon == mycon) {
             ERROR("Service %s does not have a SELinux domain defined.\n", name_.c_str());
-            free(mycon);
-            free(fcon);
-            return false;
+			// zormax
+            //free(mycon);
+            //free(fcon);
+            //return false;
         }
         free(mycon);
         free(fcon);
@@ -780,8 +781,6 @@ bool ServiceManager::ReapOneProcess() {
     } else if (pid == -1) {
         ERROR("waitpid failed: %s\n", strerror(errno));
         return false;
-    } else if (property_child_reap(pid)) {
-        return true;
     }
 
     Service* svc = FindServiceByPid(pid);
